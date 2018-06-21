103cbb6e8a199cf86b5758508e2a56d1b14da1d0
Sandra G. Alcaraz Amezcua <sandra_alcaraz.a@outlook.com>
2018-06-20 23:22:53 -0500

    struct

---
+++ crawl.go
@@ -0,0 +1,70 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 241.
+
+// Crawl2 crawls web links starting with the command-line arguments.
+//
+// This version uses a buffered channel as a counting semaphore
+// to limit the number of concurrent calls to links.Extract.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+"github.com/emirpasic/gods/trees"
+	"gopl.io/ch5/links"
+)
+
+//!+sema
+// tokens is a counting semaphore used to
+// enforce a limit of 20 concurrent requests.
+package main
+
+
+var tokens = make(chan struct{}, w)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{} // acquire a token
+	list, err := links.Extract(url)
+	<-tokens // release the token
+
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+//!-sema
+
+//!+
+func main() {
+	worklist := make(chan []string)
+	var n int // number of pending sends to worklist
+	var dep, n int
+	profundidad :=3
+	// Start with the command-line arguments.
+	n++
+	go func() {
+		 worklist <- []string {os.Args[1] }}()
+
+
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		list := <-worklist
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+}
+
+//!-
